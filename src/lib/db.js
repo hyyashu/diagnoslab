8eda3340546923cc438964c2a1d29f0f53195480
Yash Choudhary <hey.hyyashu@gmail.com>
2024-09-17 04:58:10 +0530

    fixed metadata

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,10 @@
-import { createClient } from "@supabase/supabase-js";
-import { drizzle } from "drizzle-orm/supabase";
-import { users } from "./schema";
+// import { createClient } from "@supabase/supabase-js";
+// import { drizzle } from "drizzle-orm/supabase";
+// import { users } from "./schema";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+// );
 
-export const db = drizzle(supabase, { schema: { users } });
+// export const db = drizzle(supabase, { schema: { users } });
